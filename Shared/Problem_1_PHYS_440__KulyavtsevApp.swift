c045828daccc1b36f811f172d8ce289b816b1bbb
paulina kuly <pkulyavtsev@hawk.iit.edu>
2022-01-28 14:10:24 -0600

    Initial Commit

---
+++ Shared/Problem_1_PHYS_440__KulyavtsevApp.swift
@@ -0,0 +1,17 @@
+//
+//  Problem_1_PHYS_440__KulyavtsevApp.swift
+//  Shared
+//
+//  Created by Paulina on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Problem_1_PHYS_440__KulyavtsevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
